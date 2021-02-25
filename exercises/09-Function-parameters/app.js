438a767525248cfb1eab4cf131b9434c044fa446
leisyarcia <leisyarcia@gmail.com>
2021-02-25 18:39:59 +0000

    all working

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,5 +1,7 @@
 // Your code goes here:
-
+function renderPerson (name,birth_year, eye_color,age, gender){
+    return name + " is a " + age + " years old " + gender + " born in " + birth_year + " with " + eye_color + " eyes";
+}
 
 
   ///*** Do not edit below this line ****/
